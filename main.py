0573bd293a5517cb72dcc7d61074b537ddd72df8
MarkS098 <marik.shiff@gmail.com>
2025-01-29 10:01:50 +0200

    Character levels input added

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Hello World!')
+'''
+
+'''
+
+import numpy as np
+import scipy as scp
+import matplotlib as mpl
+
+print('Enter character levels:')
+characterLevels = input().split()
+characterLevels = np.array(characterLevels, dtype=int)
+
+print('The character levels are: ',characterLevels)
+print('Party size is: ', characterLevels.size)
+
+
